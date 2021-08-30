87acec4fad6fb1184461d7075a04c816c4aac599
Cristi Petre <driimus@vivaldi.net>
2021-08-30 22:22:30 +0100

    test: no explicit disconnect

--- tests/copy11.test.js
+++ tests/copy11.test.js
@@ -1,9 +1,5 @@
 const prisma = require("../src");
 
-afterAll(async () => {
-  await prisma.$disconnect();
-});
-
 it("might leak", async () => {
   await prisma.user.deleteMany();
   const user = await prisma.user.create({
