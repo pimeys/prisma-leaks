b99d0d2c2a4488ed6d27542ab2b8a6baa8ef5499
Julius de Bruijn <julius+github@nauk.io>
2021-08-31 19:38:04 +0200

    add prisma.js

---
+++ prisma.js
@@ -0,0 +1,20 @@
+const { PrismaClient } = require("@prisma/client");
+
+async function main() {
+  var prisma = new PrismaClient();
+
+  while (true) {
+    await prisma.user.deleteMany();
+    const user = await prisma.user.create({
+      data: {
+        email: "test",
+      },
+    });
+
+    console.log(user);
+  }
+
+  await prisma.$disconnect();
+}
+
+main();
